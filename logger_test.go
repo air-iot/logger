ccea627ff43f1c2f99f82436928e8debd554fcc4
zhangqiang <z397435052@gmail.com>
2023-09-19 11:37:21 +0800

    test: 单元测试

--- logger_test.go
+++ logger_test.go
@@ -1,7 +1,49 @@
 package logger
 
-import "testing"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+	"time"
+)
 
-func TestLog(t *testing.T) {
-	Errorln(1)
+func Test_1(t *testing.T) {
+	NewLogger(Log{
+		Level:         5,
+		Output:        "file",
+		OutputFile:    "./log/test.log",
+		RotationTime:  1,
+		RotationCount: 1,
+	})
+	for i := 0; i < 100; i++ {
+		Debugln(i)
+	}
+
+	time.Sleep(time.Minute * 2)
+	for i := 0; i < 100; i++ {
+		Debugln(i)
+	}
+	time.Sleep(time.Minute * 2)
+	for i := 0; i < 100; i++ {
+		Debugln(i)
+	}
+	println("结束")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-sc
+}
+
+func BenchmarkLogger(b *testing.B) {
+	NewLogger(Log{
+		Level:         5,
+		Output:        "file",
+		OutputFile:    "./log/test.log",
+		RotationTime:  1,
+		RotationCount: 1,
+	})
+	for i := 0; i < b.N; i++ {
+		Debugln(i)
+	}
+
 }
